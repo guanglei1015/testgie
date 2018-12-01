3ef108666b374056cf750fea2f3d88c94bf940af
guanglei1015 <3268188701@qq.com>
2018-12-01 20:48:25 +0800

    eclipse second

---
+++ src/com/test/Git01.java
@@ -0,0 +1,8 @@
+package com.test;
+public class Git01 {
+
+    public static void main(String[] args) {
+        System.out.println("hi git");
+        System.out.println("============2222222222222");
+    }
+}
