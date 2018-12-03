fc3768bea87426d2f17c41b399e1ca646c8306b7
guanglei1015 <3268188701@qq.com>
2018-12-03 15:17:54 +0800

    eclipse add tt() 5;

--- src/com/test/Git01.java
+++ src/com/test/Git01.java
@@ -5,5 +5,11 @@ public class Git01 {
         System.out.println("hi git");
         System.out.println("============2222222222222");
         System.out.println("333333333333");
+        tt();
+    }
+    
+    public static void tt(){
+    	
+    	System.out.println("1234567889");
     }
 }
