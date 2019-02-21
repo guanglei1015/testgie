d7e358d6031c17f232552ab31c952f3c08d41d03
guanglei1015 <3268188701@qq.com>
2019-02-21 22:07:56 +0800

    six commit directory new branch

--- mavengitTest/src/main/java/com/bjpowenode/GitTest02.java
+++ mavengitTest/src/main/java/com/bjpowenode/GitTest02.java
@@ -4,5 +4,6 @@ public class GitTest02 {
     public static void main(String[] args) {
         System.out.println("four new branch==== new GitTest02");
         System.out.println("fire new branch==== ");
+        System.out.println("six new branch==== ");
     }
 }
