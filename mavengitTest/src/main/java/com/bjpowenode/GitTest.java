13e9ba69a52ca74949c969d9a52da76fb16e9548
guanglei1015 <3268188701@qq.com>
2019-02-21 21:29:19 +0800

    second commit

--- mavengitTest/src/main/java/com/bjpowenode/GitTest.java
+++ mavengitTest/src/main/java/com/bjpowenode/GitTest.java
@@ -3,5 +3,7 @@ package com.bjpowenode;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("=====Hi git=====");
+        System.out.println("+++++HI  Second+++++++");
+
     }
 }
