4a7b3ea61a4cdad3917140bace6f583344623d9a
guanglei1015 <3268188701@qq.com>
2019-02-21 22:03:01 +0800

    for commit  new branch

--- mavengitTest/src/main/java/com/bjpowenode/GitTest02.java
+++ mavengitTest/src/main/java/com/bjpowenode/GitTest02.java
@@ -3,5 +3,6 @@ package com.bjpowenode;
 public class GitTest02 {
     public static void main(String[] args) {
         System.out.println("four new branch==== new GitTest02");
+        System.out.println("fire new branch==== ");
     }
 }
